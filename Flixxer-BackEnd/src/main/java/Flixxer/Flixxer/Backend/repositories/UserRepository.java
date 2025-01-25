5b6fd1e26e35306a10977174423f4f1fdb2d5fe8
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2025-01-25 14:37:53 -0500

    commit any changes

--- Flixxer-BackEnd/src/main/java/Flixxer/Flixxer/Backend/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package Flixxer.Flixxer.Backend.repositories;
-
-import Flixxer.Flixxer.Backend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
